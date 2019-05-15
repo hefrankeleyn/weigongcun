7790c0745f4d3359acfa00694eba2bf287c56746
Li Fei <hefrankeleyn@gamil.com>
2019-05-15 13:56:16 +0800

    项目初始化

---
+++ EdmFetchDataPlatform/src/main/java/com/edm/edmfetchdataplatform/EdmfetchdataplatformApplication.java
@@ -0,0 +1,13 @@
+package com.edm.edmfetchdataplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdmfetchdataplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdmfetchdataplatformApplication.class, args);
+    }
+
+}
