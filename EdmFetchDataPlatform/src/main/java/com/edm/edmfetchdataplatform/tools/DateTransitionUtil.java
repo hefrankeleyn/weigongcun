272c738c8f0bbc06a2b026f1a1b0d7901791edc7
Li Fei <hefrankeleyn@gamil.com>
2019-05-16 15:41:20 +0800

    时间类型转换工具类

---
+++ EdmFetchDataPlatform/src/main/java/com/edm/edmfetchdataplatform/tools/DateTransitionUtil.java
@@ -0,0 +1,22 @@
+package com.edm.edmfetchdataplatform.tools;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Date 2019-05-16
+ * @Author lifei
+ */
+public class DateTransitionUtil {
+
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+
+    /**
+     * 将时间类型转换成字符串
+     * @param date
+     * @return
+     */
+    public static String toDateStr(Date date){
+        return simpleDateFormat.format(date);
+    }
+}
