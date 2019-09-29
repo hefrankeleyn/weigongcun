1a6ebe811754f6441161321ef7c77901edf2bdd6
Li Fei <hefrankeleyn@gamil.com>
2019-09-29 09:53:31 +0800

    重新将数据拼成一行

---
+++ DataClearningMRLogFormalWithEquality/src/main/java/com/hef/mr/utils/KeyCompareValueToLineUtil.java
@@ -0,0 +1,29 @@
+package com.hef.mr.utils;
+
+import java.util.Map;
+
+/**
+ * @Date 2019-09-28
+ * @Author lifei
+ */
+public class KeyCompareValueToLineUtil {
+
+    /**
+     * 将key 和value 拼成行
+     * @param keys
+     * @param kv
+     * @return
+     */
+    public static String keysCompareValuesToStr(String[] keys, Map<String, String> kv){
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<keys.length; i++){
+            if (kv.containsKey(keys[i].toLowerCase().trim())){
+                sb.append(kv.get(keys[i].toLowerCase().trim()));
+            }
+            if (i<keys.length-1){
+                sb.append("\t");
+            }
+        }
+        return sb.toString();
+    }
+}
